b5c3ebe57aee5056b29b60c36c2601c922571640
Soreine <soreine.plume@gmail.com>
2018-11-10 11:28:48 +0100

    Can use controller to move player

--- Assets/Scenes/Nico Room/Scripts/PlayerController.cs
+++ Assets/Scenes/Nico Room/Scripts/PlayerController.cs
@@ -7,16 +7,23 @@ public class PlayerController : MonoBehaviour
     public float forwardSpeed;
     public float turnSpeed;
 
+    // Track the last direction the player moved to. 
+    // The model will align itself with this direction progressively
+    private Vector3 movementDirection;
+
     void Update()
     {
-        float turnInput = Input.GetAxis("Horizontal");
-        float turnAmount = turnInput * turnSpeed * Time.deltaTime;
-        transform.RotateAround(transform.position, Vector3.up, turnAmount);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        movementDirection = new Vector3(horizontalInput, 0, verticalInput);
 
-        float forwardInput = Input.GetAxis("Vertical");
-        float moveAmount = forwardInput * forwardSpeed * Time.deltaTime;
+        float moveAmount = forwardSpeed * Time.deltaTime;
+        transform.position = transform.position + (movementDirection * moveAmount);
 
-        Vector3 direction = transform.forward;
-        transform.Translate(Vector3.forward * moveAmount);
+        // Slowly make the model orient to the current direction
+        // TODO: Adjust with Time.deltaTime
+        Vector3 newDirection = Vector3.Slerp(transform.forward, movementDirection, turnSpeed);
+
+        transform.rotation = Quaternion.LookRotation(newDirection);
     }
 }
