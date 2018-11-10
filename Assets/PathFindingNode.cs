0cd2ee61e175c82e888affb8624101eb56f2bfc9
Akamis <mrtetex@gmail.com>
2018-11-10 12:33:18 +0100

    Update NavMesh

---
+++ Assets/PathFindingNode.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFindingNode : MonoBehaviour {
+
+	[Header("Pathfindind")]
+	public int nodeNumber;
+
+	[Header("Gizmos")]
+	public Color color;
+	public float radius = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	/// <summary>
+	/// Callback to draw gizmos that are pickable and always drawn.
+	/// </summary>
+	void OnDrawGizmos()
+	{
+		Gizmos.color = color;
+		Gizmos.DrawSphere(transform.position, radius);
+	}
+}
