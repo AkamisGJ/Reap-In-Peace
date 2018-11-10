9415e35df38e86388bdacb477edb4f547d35358c
Soreine <soreine.plume@gmail.com>
2018-11-10 13:47:01 +0100

    Try using SmoothDamp

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,54 +1,65 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
     // Keep reference to the camera in order to make movement
     // relative to the current camera view.
     public Camera camera;
 
     public float forwardSpeed;
-    public float turnSpeed;
+    // Approximate time it takes for the character 
+    // to rotate to the last movement direction
+    public float rotationTime;
+    // Maximum rotation speed;
+    public float maxRotationSpeed;
 
     // Track the last direction the player moved to. 
     // The model will align itself with this direction progressively
     private Vector3 lastMovementDirection;
 
-    Vector3 getCameraDirection()
-    {
+    private Vector3 currentRotationVelocity;
+
+    Vector3 getCameraDirection () {
         Vector3 cameraDirection = camera.transform.forward;
-        Vector3 planarCameraDirection = new Vector3(
+        Vector3 planarCameraDirection = new Vector3 (
             cameraDirection.x,
             0, // Discard the vertical axis
             cameraDirection.z
         );
 
-        return Vector3.Normalize(planarCameraDirection);
+        return Vector3.Normalize (planarCameraDirection);
     }
 
-    void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 cameraForward = getCameraDirection();
-        Vector3 cameraRight = Vector3.Cross(Vector3.up, cameraForward);
+    void Update () {
+        float horizontalInput = Input.GetAxis ("Horizontal");
+        float verticalInput = Input.GetAxis ("Vertical");
+        Vector3 cameraForward = getCameraDirection ();
+        Vector3 cameraRight = Vector3.Cross (Vector3.up, cameraForward);
         Vector3 movementDirection = cameraForward * verticalInput + cameraRight * horizontalInput;
 
         float moveAmount = forwardSpeed * Time.deltaTime;
         Vector3 translation = movementDirection * moveAmount;
         transform.position += translation;
 
-        if (movementDirection.magnitude > 0)
-        {
+        if (movementDirection.magnitude > 0) {
             // Store the last direction of any actual movement done.
-            lastMovementDirection = Vector3.Normalize(movementDirection);
+            lastMovementDirection = Vector3.Normalize (movementDirection);
         }
 
         // Slowly make the model orient to the current direction
-        // TODO: Adjust with Time.deltaTime
-        Vector3 newDirection = Vector3.Slerp(transform.forward, lastMovementDirection, turnSpeed);
+        Vector3 newDirection = Vector3.SmoothDamp (
+            // current
+            transform.forward,
+            // target
+            lastMovementDirection,
+            ref currentRotationVelocity,
+            // smoothTime
+            rotationTime
+            // maxRotationSpeed,
+            // Time.deltaTime
+        );
 
-        transform.rotation = Quaternion.LookRotation(newDirection);
+        transform.rotation = Quaternion.LookRotation (newDirection);
     }
 }
