6fe270f714e8466396acb75d5719fffc298edc3e
Akamis <mrtetex@gmail.com>
2018-11-10 01:30:41 +0100

    Git Init
    Le retour de Unity en version stable !

---
+++ Assets/Scenes/Theo Room/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.position + offset;
+		
+	}
+}
