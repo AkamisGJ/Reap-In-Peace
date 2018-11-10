cc7c57ae510cd9decab2afda5bd982181a41ef2c
Soreine <soreine.plume@gmail.com>
2018-11-10 13:31:04 +0100

    Keep rotating after movement is done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,20 +13,18 @@ public class PlayerController : MonoBehaviour
 
     // Track the last direction the player moved to. 
     // The model will align itself with this direction progressively
-    private Vector3 movementDirection;
+    private Vector3 lastMovementDirection;
 
     Vector3 getCameraDirection()
     {
         Vector3 cameraDirection = camera.transform.forward;
-        Vector3 planarCameraDirection = Vector3.Normalize(
-            new Vector3(
-                cameraDirection.x,
-                0, // Discard the vertical axis
-                cameraDirection.z
-            )
+        Vector3 planarCameraDirection = new Vector3(
+            cameraDirection.x,
+            0, // Discard the vertical axis
+            cameraDirection.z
         );
 
-        return planarCameraDirection;
+        return Vector3.Normalize(planarCameraDirection);
     }
 
     void Update()
@@ -35,14 +33,21 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 cameraForward = getCameraDirection();
         Vector3 cameraRight = Vector3.Cross(Vector3.up, cameraForward);
-        movementDirection = cameraForward * verticalInput + cameraRight * horizontalInput;
+        Vector3 movementDirection = cameraForward * verticalInput + cameraRight * horizontalInput;
 
         float moveAmount = forwardSpeed * Time.deltaTime;
-        transform.position = transform.position + (movementDirection * moveAmount);
+        Vector3 translation = movementDirection * moveAmount;
+        transform.position += translation;
+
+        if (movementDirection.magnitude > 0)
+        {
+            // Store the last direction of any actual movement done.
+            lastMovementDirection = Vector3.Normalize(movementDirection);
+        }
 
         // Slowly make the model orient to the current direction
         // TODO: Adjust with Time.deltaTime
-        Vector3 newDirection = Vector3.Slerp(transform.forward, movementDirection, turnSpeed);
+        Vector3 newDirection = Vector3.Slerp(transform.forward, lastMovementDirection, turnSpeed);
 
         transform.rotation = Quaternion.LookRotation(newDirection);
     }
