fc1218ae2c67ef372fdab938393b857f7c191e2a
Eva Gt-Gd <eva.gerbert.gaillard@gmail.com>
2018-11-10 11:44:25 +0100

    Swinging spot light

---
+++ Assets/Scripts/DetectLight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Visibility v = other.GetComponent<Visibility>();
+
+            if(v != null)
+            {
+                v.SetVisibility(true);
+            }            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Visibility v = other.GetComponent<Visibility>();
+
+            if (v != null)
+            {
+                v.SetVisibility(false);
+            }
+        }
+    }
+}
