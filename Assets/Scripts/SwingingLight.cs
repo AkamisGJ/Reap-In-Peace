747346a7def3fa9ffe4382b3a9720e10f9098041
Eva Gt-Gd <eva.gerbert.gaillard@gmail.com>
2018-11-10 15:56:47 +0100

    Fix Light rotation

--- Assets/Scripts/SwingingLight.cs
+++ Assets/Scripts/SwingingLight.cs
@@ -12,16 +12,16 @@ public class SwingingLight : MonoBehaviour {
     [Range(0,10)]
     public float swingingSpeed;
 
-    //Vector3 positionOfRotation;
+    Quaternion initRotation;
 
     // Use this for initialization
     void Start () {
-        //positionOfRotation = gameObject.transform.parent.transform.position;
+        initRotation = transform.localRotation;
     }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 rotation = axeOfRotation * Mathf.Clamp(maxAngle * Mathf.Sin(Time.time * swingingSpeed), -clampAngle, clampAngle);
-        gameObject.transform.parent.transform.localRotation = Quaternion.Euler(rotation);
+        transform.localRotation = initRotation * Quaternion.Euler(rotation);
     }
 }
