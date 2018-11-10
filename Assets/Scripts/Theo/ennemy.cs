eebc9c1eb43d80eb6f3956e247399abcb9853727
Soreine <soreine.plume@gmail.com>
2018-11-10 22:21:35 +0100

    Add support for back and forth or not

--- Assets/Scripts/Theo/ennemy.cs
+++ Assets/Scripts/Theo/ennemy.cs
@@ -6,28 +6,30 @@ using UnityEngine.AI;
 public class ennemy : MonoBehaviour {
 
 	public GameObject PathFinding;
-	public int startnode;
-	public bool clockwise;
+	public bool backAndForth = true;
 	public bool findplayer = false;
 
-	public int nodetogo;
+	// Index of the next node
+	private int nodetogo = 0;
+	// +1 when going over nodes in order. -1 when going in reverse order
+	private int step = +1;
+
 	private Vector3 target;
 	private PathFindingNode[] nodes;
 	private NavMeshAgent navMeshAgent;
 
 	// Use this for initialization
 	void Start () {
-		nodes = PathFinding.GetComponentsInChildren<PathFindingNode>();
-		nodetogo = startnode;
-		SetTarget();
-		navMeshAgent = GetComponent<NavMeshAgent>();
-		print(nodes.Length);
+		nodes = PathFinding.GetComponentsInChildren<PathFindingNode> ();
+		SetTarget ();
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		print (nodes.Length);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if(findplayer == false){
-			navMeshAgent.SetDestination(target);
+		if (findplayer == false) {
+			navMeshAgent.SetDestination (target);
 		}
 	}
 
@@ -35,22 +37,33 @@ public class ennemy : MonoBehaviour {
 	/// OnTriggerEnter is called when the Collider other enters the trigger.
 	/// </summary>
 	/// <param name="other">The other Collider involved in this collision.</param>
-	void OnTriggerEnter(Collider other)
-	{
-		if(findplayer == false){
-			if(other.tag == "Node" && other.GetComponent<PathFindingNode>().nodeNumber == nodetogo){
-				if(nodetogo < nodes.Length - 1 && clockwise == true){
-					nodetogo++;
-					SetTarget();
-				}else{
-					nodetogo = 0;
-					SetTarget();
+	void OnTriggerEnter (Collider other) {
+		// Ignore encountering nodes if chasing the player
+		if (findplayer) return;
+
+		if (other.tag != "Node") return;
+
+		Collider target = nodes[nodetogo].GetComponent<Collider> ();
+
+		if (other == target) {
+			// Switch to next path node
+			int next = nodetogo + step;
+			if (next == -1 || next == nodes.Length) {
+				// We found the last node.
+				if (backAndForth) {
+					step = -step;
+					next = nodetogo + step;
+				} else {
+					next = 0;
 				}
 			}
+
+			nodetogo = next;
+			SetTarget ();
 		}
 	}
 
-	void SetTarget(){
+	void SetTarget () {
 		target = nodes[nodetogo].transform.position;
 	}
 }
