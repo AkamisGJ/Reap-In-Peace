145f4531accc29a0c88a768b344e86c4c120091f
Akamis <mrtetex@gmail.com>
2018-11-11 15:01:55 +0100

    Update Boss

---
+++ Assets/oldboss.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class oldboss : MonoBehaviour {
+	public float max_velocity = 15f;
+	public float velocity;
+
+	// Update is called once per frame
+	void OnTriggerStay(Collider other)
+	{
+		var playerpos = other.transform.position;
+		var desired_velocity = Vector3.Normalize(playerpos - transform.position) * max_velocity;
+		transform.Translate(desired_velocity * velocity * Time.deltaTime);
+	}
+}
