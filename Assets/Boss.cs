dc98fb75759a38ab5570b79753cfd6a3e87671c4
Akamis <mrtetex@gmail.com>
2018-11-11 11:39:12 +0100

    Add GameControlleur

--- Assets/Boss.cs
+++ Assets/Boss.cs
@@ -6,15 +6,25 @@ public class Boss : MonoBehaviour {
 
 	public float max_velocity = 15f;
 	public float velocity;
-	void Start () {
-		
-	}
+	public Transform[] pathfinding;
 	
 
 	void OnTriggerStay(Collider other)
 	{
-		var playerpos = other.transform.position;
-		var desired_velocity = Vector3.Normalize(playerpos - transform.position) * max_velocity;
-		transform.Translate(desired_velocity * velocity * Time.deltaTime);
+		Vector3 farest = Vector3.zero;
+
+		for (int i = 0; i < pathfinding.Length; i++)
+		{
+			var node = pathfinding[i];
+			var distanceBoss = (transform.position + node.position).sqrMagnitude;
+			Debug.DrawLine(transform.position, node.position);
+			print(distanceBoss + node.name);
+
+			if(distanceBoss > farest.sqrMagnitude){
+				farest = node.position;
+			}
+		}
+		print("Farest = " + farest);
+
 	}
 }
